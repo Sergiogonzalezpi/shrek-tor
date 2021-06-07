9ce79e634e992de55d640ffe4206c6fff2297fa8
Sergiogonzalezpi <sergio.gonzalezpi@alumnos.upm.es>
2021-06-07 11:22:55 +0200

    upload modified protocol control

--- shrek_protocols/protocol_control.py
+++ shrek_protocols/protocol_control.py
@@ -4,6 +4,8 @@ import os
 import json
 import socket
 import random
+import base64
+
 
 # Network Configuration - Control Protocol
 
@@ -15,11 +17,11 @@ def keep_ip(route_config_file, ip, port):
     json_data = json.loads(str(file_data))
     json_data = dict(json_data)
     host_index = len(json_data['host'])
-    if(host_index != 0):
-        for position in range(0,host_index):
-            if(json_data['host'][position]['ip'] == ip):
+    if host_index != 0:
+        for position in range(0, host_index):
+            if json_data['host'][position]['ip'] == ip:
                 exists = True
-    if(exists == False):
+    if not exists:
         json_data['host'].append({})
         json_data['host'][host_index]['ip'] = ip
         json_data['host'][host_index]['port'] = port
@@ -27,6 +29,7 @@ def keep_ip(route_config_file, ip, port):
         file = open(route_config_file, 'w')
         file.write(json_obj + '\n')
         file.close()
+
 
 def set_info_host(route_config_file, port):
     file = open(route_config_file, 'rb')
@@ -42,6 +45,7 @@ def set_info_host(route_config_file, port):
     file.write(json_obj + '\n')
     file.close()
 
+
 def get_info_host(route_config_file, opc):
     file = open(route_config_file, 'rb')
     file_data = file.read()
@@ -50,17 +54,26 @@ def get_info_host(route_config_file, opc):
     json_data = dict(json_data)
     ip = json_data[str(opc)]['ip']
     port = json_data[str(opc)]['port']
-    return (ip,port)
+    return (ip, port)
 
-def json_toString(json_file):
-    file = open(json_file, 'rb')
+
+# Tunneling Network - Control Protocol
+
+def add_host_to_route(route_config_file, ip, port):
+    file = open(route_config_file, 'rb')
     file_data = file.read()
     file.close()
     json_data = json.loads(str(file_data))
     json_data = dict(json_data)
-    return str(json_data)
+    index = len(json_data['hosts'])
+    json_data['hosts'].append({})
+    json_data['hosts'][index]['ip'] = ip
+    json_data['hosts'][index]['port'] = port
+    json_obj = json.dumps(json_data, indent=4)
+    file = open(route_config_file, 'w')
+    file.write(json_obj + '\n')
+    file.close()
 
-# Tunneling Network - Control Protocol
 
 def set_instructions_tunnel(route_config_file):
     exists = False
@@ -69,7 +82,7 @@ def set_instructions_tunnel(route_config_file):
     file.close()
     json_data = json.loads(str(file_data))
     json_data = dict(json_data)
-    host_index = len(json_data['host'])
+    host_index = len(json_data['hosts'])
 
 
 # Load config files - Node Protocol
@@ -77,7 +90,28 @@ def set_instructions_tunnel(route_config_file):
 def get_config_files():
     execution('wget https://raw.githubusercontent.com/Sergiogonzalezpi/shrek-tor/main/hostconfig.json')
 
-# Execution commands
+
+# Utilities
+
+#   Read and write files
+
+def files(file_input, wr, message = ''):
+    if str(wr) == 'write':
+        opc = 'w'
+    else:
+        opc = 'rb'
+    file = open(file_input, opc)
+    if str(wr) == 'write':
+        file.write(message)
+        file.close()
+    else:
+        file_data = file.read()
+        file.close()
+        return file_data
+    return 'Close file ...'
+
+
+#   Execution commands
 
 def execution(command):
     try:
@@ -87,3 +121,23 @@ def execution(command):
     except:
         print('Failed command')
         return False
+
+
+#   String formatting - Node Protocol
+
+def json_toString(json_file):
+    file = open(json_file, 'rb')
+    file_data = file.read()
+    file.close()
+    json_data = json.loads(str(file_data))
+    json_data = dict(json_data)
+    return str(json_data)
+
+
+def str_base64(str_n):
+    return base64.b64encode(str(str_n))
+
+
+def base64_str(base_str):
+    return base64.b64decode(str(base_str))
+
